f0e96fb45cab6c5140463543e3fe6bbe74fcde6f
Kate <ktvlphoto@outlook.com>
2023-07-17 22:43:48 +0300

    task: remove

--- src/Task.java
+++ src/Task.java
@@ -4,6 +4,9 @@ public class Task {
     public static void main(String[] args) {
         String[] arr = {"посмотри", "слева", "справа", "сзади"};
         smash(arr);
+
+        String str = "hello";
+        System.out.println(remove(str));
     }
 
     //Напишите функцию, которая принимает массив слов, объединяет их в предложение и возвращает это предложение.
@@ -11,6 +14,13 @@ public class Task {
         String arrayToString = String.join(" ", words);
         System.out.println(arrayToString);
         return arrayToString;
+    }
+
+
+    // создать функцию, которая удаляет первый и последний символы строки
+    public static String remove(String str) {
+    String str2 = str.substring(1, str.length() - 1);
+        return str2;
     }
 }
 
