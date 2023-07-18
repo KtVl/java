f2efcf33fd3d9c7d97bf408341b1ceddc89e0624
Kate <ktvlphoto@outlook.com>
2023-07-18 21:31:12 +0300

    findAverage

--- src/Codewars.java
+++ src/Codewars.java
@@ -1,12 +1,13 @@
-import java.util.Arrays;
-
-public class Task {
+public class Codewars {
     public static void main(String[] args) {
         String[] arr = {"посмотри", "слева", "справа", "сзади"};
         smash(arr);
 
         String str = "hello";
         System.out.println(remove(str));
+
+        int[] array = {1,2,3,6,5};
+        System.out.println(findAverage(array));
     }
 
     //Напишите функцию, которая принимает массив слов, объединяет их в предложение и возвращает это предложение.
@@ -19,8 +20,20 @@ public class Task {
 
     // создать функцию, которая удаляет первый и последний символы строки
     public static String remove(String str) {
-    String str2 = str.substring(1, str.length() - 1);
+        String str2 = str.substring(1, str.length() - 1);
         return str2;
+    }
+
+
+    // Напишите функцию, которая вычисляет среднее значение чисел в заданном списке.
+    public static double findAverage(int[] array) {
+        double size = array.length;
+        double sum = 0;
+        for (int i = 0; i < array.length; i++) {
+            sum = sum + array[i];
+        }
+        double avg = sum / size;
+        return avg;
     }
 }
 
